44039cd0b31bc5654d2486e4f71e624dee8b3636
AnnaBeumer <anna.m.beumer@gmail.com>
2021-11-02 21:42:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+import Student from "./Components/Student";
+import Opdracht from "./Components/Opdracht";
+import Home from "./Components/Home";
+// import Utils from "./utils";
+
+const App = () => {
+  // Utils();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/student">Studenten</Link>
+            </li>
+            <li>
+              <Link to="/opdracht">Opdrachten</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/student">
+              <Student />
+            </Route>
+            <Route path="/opdracht">
+              <Opdracht />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
